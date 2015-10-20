581b35c852e3e196b255ad059325fc86123de6ef
Bede <bede.overend@gmail.com>
2015-10-20 19:09:47 +1100

    call parent _setDefaultElement

--- src/default-content.js
+++ src/default-content.js
@@ -2,6 +2,17 @@ class DefaultContent {
   beforeRegister() {
     this.is = 'default-content';
   }
+
+  ready() {
+    let parent = this.parentElement;
+
+    if (!parent || typeof parent._setDefaultElement !== 'function') {
+      console.warn('The default-content element must be used within an element implementing _setDefaultElement');
+      return;
+    };
+
+    parent._setDefaultElement(this);
+  }
 }
 
 Polymer(DefaultContent);
